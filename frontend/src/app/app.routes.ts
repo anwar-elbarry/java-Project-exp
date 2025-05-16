1dd1f2d3ff4b0deeae3264ff22fd4ce3c4fa7dc2
Anouar El barry <elbarryanwar37@gmail.com>
2025-05-16 16:47:21 +0100

    feat: define application routes for user and book components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { BookListComponent } from './components/book-list/book-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'users',
+        pathMatch: 'full'
+    },
+    {
+        path: 'users',
+        component: UserListComponent
+    },
+    {
+        path : 'books',
+        component : BookListComponent
+    }
+];
