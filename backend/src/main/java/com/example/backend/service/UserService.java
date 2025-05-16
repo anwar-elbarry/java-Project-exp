76db1c132d304484c72121a1611252fc7a104a63
Anouar El barry <elbarryanwar37@gmail.com>
2025-05-16 11:44:52 +0100

    feat: implement UserService for user management with methods to find, create, and delete users

---
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -0,0 +1,35 @@
+package com.example.backend.service;
+import com.example.backend.repositories.UserRepository;
+import com.example.backend.entities.User;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User findByUserEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+
+    public void deletUser(Long id){
+       userRepository.deleteById(id);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
