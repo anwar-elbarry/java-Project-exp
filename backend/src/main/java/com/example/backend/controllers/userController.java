28dbbb404f72bf74faa471df784cf4e88677c7cb
Anouar El barry <elbarryanwar37@gmail.com>
2025-05-16 15:19:22 +0100

    feat: enhance userController to support user DTO and improve user creation response

--- backend/src/main/java/com/example/backend/controllers/userController.java
+++ backend/src/main/java/com/example/backend/controllers/userController.java
@@ -7,34 +7,46 @@ import java.util.Map;
 import com.example.backend.entities.User;
 import com.example.backend.service.UserService;
 
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import com.example.backend.dto.user_dto.userDto;
+import org.modelmapper.ModelMapper;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/users")
 @CrossOrigin(origins = "http://localhost:4200")
 public class userController {
     private final UserService userService;
+    private final ModelMapper modelMapper;
 
-    public userController(UserService userService){
+    public userController(UserService userService, ModelMapper modelMapper){
         this.userService = userService;
+        this.modelMapper = modelMapper;
     }
     @RequestMapping("/all")
-    public List<User> getAll(){
-        return userService.findAll();
+    public List<userDto> getAll(){
+        List<User> users = userService.findAll();
+        return users.stream()
+                    .map(user -> modelMapper.map(user, userDto.class))
+                    .collect(Collectors.toList());
     }
     
     @PostMapping("/create")
-    public ResponseEntity<Map<String, Object>> createUser(@RequestBody User user) {
+    public ResponseEntity<Map<String, Object>> createUser(@Valid @RequestBody userDto userDto) {
+        User user = modelMapper.map(userDto, User.class);
         User createdUser = userService.createUser(user);
+        userDto responseUser = modelMapper.map(createdUser,userDto.class);
+
         Map<String, Object> response = new HashMap<>();
         response.put("message", "User created successfully");
-        response.put("user", createdUser);
+        response.put("user", responseUser);
         return ResponseEntity.ok(response);
     }
 }
